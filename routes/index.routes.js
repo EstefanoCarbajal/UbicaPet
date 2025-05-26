84361c599c4c744467a36abc255cc611c3dd3cae
EstefanoCarbajal <tefa3099@hotmail.com>
2025-05-25 23:23:58 -0500

    Hasta la prueba index

---
+++ routes/index.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// Ruta principal (http://localhost:3000/)
+router.get('/', (req, res) => {
+  res.render('index', { titulo: 'Bienvenido a UbicaPet' });
+});
+
+module.exports = router;
